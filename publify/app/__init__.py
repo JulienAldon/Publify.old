f4ef7329a493392747b099287bdf6d785c3a1278
Julien <julien.aldon@epitech.eu>
2021-01-13 14:47:16 +0100

    Add wildcard Access-Control-Allow-Origin

--- publify/app/__init__.py
+++ publify/app/__init__.py
@@ -6,8 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-cors = CORS(app,
-    supports_credentials=True, max_age=86400)
+cors = CORS(app, supports_credentials=True, max_age=86400, send_wildcard=True)
 db = SQLAlchemy(app)
 
 
