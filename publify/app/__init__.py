6eb228268b8e286faca8e29f50b04c9d5faaec36
Julien <julien.aldon@epitech.eu>
2021-01-13 14:53:44 +0100

    cord

--- publify/app/__init__.py
+++ publify/app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-cors = CORS(app, supports_credentials=True, max_age=86400, origins=['auth.publify.aldon.info', 'publify.aldon.info'])
+cors = CORS(app, supports_credentials=True, max_age=86400, origins=['auth.publify.aldon.info', 'publify.aldon.info'], ressources=r'/api/*')
 db = SQLAlchemy(app)
 
 
