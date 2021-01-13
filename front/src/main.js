94d52d98bd9cda235615ff291838ecd99bb74d6c
Julien <julien.aldon@epitech.eu>
2021-01-13 14:35:57 +0100

    revert to normal

--- front/src/main.js
+++ front/src/main.js
@@ -1,27 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
 // import store from './store/index';
 import VModal from 'vue-js-modal';
 
-Vue.use(VueRouter)
 Vue.use(VModal, { dialog: true });
 
-
-const routes = [
-  {path: '/', component: { template: '<p>home page</p>'}},
-  {path: '/app', component: App}
-]
-
-const router = new VueRouter({
-  routes
-})
-
 var vm = new Vue({
-  router
+  render: h => h(App),
 }).$mount('#app')
 
 global.vm = vm;
