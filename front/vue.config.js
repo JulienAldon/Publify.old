218cec861c1be405e1558c72f142fc6127cf9b50
Julien <julien.aldon@epitech.eu>
2021-01-13 18:38:07 +0100

    cors

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  devServer: {
-      disableHostCheck: true,
-      proxy: 'http://127.0.0.1:2000',
-      // port: 4000,
-      public: 'front.localhost',
-      sockHost: 'front.localhost'
-  },
-  // publicPath: "/"
+  // devServer: {
+  //     disableHostCheck: true,
+  //     proxy: 'http://127.0.0.1:2000',
+  //     // port: 4000,
+  //     public: 'front.localhost',
+  //     sockHost: 'front.localhost'
+  // },
+  publicPath: "/"
 }
