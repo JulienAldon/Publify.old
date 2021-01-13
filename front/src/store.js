af5b261d8d67cef15166154cc5d4571c2ad0ad58
Julien <julien.aldon@epitech.eu>
2021-01-13 20:00:20 +0100

    cors

--- front/src/store.js
+++ front/src/store.js
@@ -11,7 +11,7 @@ export const mutations = {
     deleteUserPlaylistLink(id) {
         fetch('http://auth.publify.aldon.info/api/v1/playlist/'+id, {
             headers: {
-                // "Access-Control-Allow-credentials": true,
+                "Access-Control-Allow-credentials": true,
             },
             credentials: "include",
             method: 'delete',
@@ -37,7 +37,7 @@ export const mutations = {
         fetch('http://auth.publify.aldon.info/api/v1/playlist', {
             headers: {
                 "Content-Type": "application/json",
-                // 'Access-Control-Allow-Credentials': true
+                'Access-Control-Allow-Credentials': true
             },
             credentials: "include",
             method: 'post',
@@ -58,7 +58,7 @@ export const mutations = {
     setUserPlaylistLinks() {
         fetch ('http://auth.publify.aldon.info/api/v1/playlist/all/links', {
             headers: {
-                    'Access-Control-Allow-Credentials': true
+                    // 'Access-Control-Allow-Credentials': true
             },
             credentials: "include"
         })
@@ -78,7 +78,7 @@ export const mutations = {
     setUserPlaylist() {
         fetch ('http://auth.publify.aldon.info/api/v1/playlist', {
             headers: {
-                    // 'Access-Control-Allow-Credentials': true
+                    'Access-Control-Allow-Credentials': true
             },
             credentials: "include"
         })
@@ -103,7 +103,7 @@ export const mutations = {
         };
         fetch('http://auth.publify.aldon.info/api/v1/playlist/'+id+'/sync', {
             headers: {
-                // "Access-Control-Allow-credentials": true,
+                "Access-Control-Allow-credentials": true,
                 "Content-Type": "application/json"
             },
             credentials: "include",
