b6e7d88f249558633acacfc9d8a99b8a0400b0c2
Julien <julien.aldon@epitech.eu>
2021-01-13 16:25:38 +0100

    cors

--- publify/app/api.py
+++ publify/app/api.py
@@ -7,7 +7,6 @@ from flask_restful.reqparse import RequestParser
 from app.models import User, PlaylistLink
 from flask_sqlalchemy import SQLAlchemy
 import app.spotifyWrapper as abc
-from flask_cors import CORS, cross_origin
 from authlib.integrations.flask_client import OAuth
 # from flask_oauthlib.client import OAuth
 import json
@@ -16,64 +15,6 @@ import functools
 #initialisation
 # app.config.from_object(Config)
 oauth = OAuth(app)
-
-from datetime import timedelta
-from flask import make_response, request, current_app
-from functools import update_wrapper
-
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    # use str instead of basestring if using Python 3.x
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    # use str instead of basestring if using Python 3.x
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
 
 def fetch_spotify_token():
 	# if not 'access_token' in fk.session:
@@ -127,7 +68,6 @@ class Radio(Resource):
 		return {}
 
 class Playlists(Resource):
-	@crossdomain(origin='*')
 	def get(self):
 		try:
 			usr = spotify.get('me').json()
@@ -148,7 +88,6 @@ class Playlists(Resource):
 		}
 		return fk.jsonify(result)
 
-	@crossdomain(origin='*')
 	def post(self):
 		try:
 			usr = spotify.get('me').json()
@@ -180,7 +119,6 @@ class Playlists(Resource):
 			})
 
 class PlaylistLinks(Resource):
-	@crossdomain(origin='*')
 	def get(self, id):
 		try:
 			usr = spotify.get('me').json()
@@ -191,7 +129,6 @@ class PlaylistLinks(Resource):
 			return createError('Not Found', 404)
 		return fk.jsonify({'collaborative': p.collab_playlist_id, 'public': p.public_playlist_id})	
 	
-	@crossdomain(origin='*')
 	def put(self, id):
 		if not 'access_token' in fk.session:
 			return createError('Unauthorized', 401, special='Authentification required')
@@ -204,7 +141,6 @@ class PlaylistLinks(Resource):
 		db.session.commit()
 		return fk.jsonify({'success': 'Playlist Link Updated'})
 
-	@crossdomain(origin='*')
 	def delete(self, id):
 		if not 'access_token' in fk.session:
 			return createError('Unauthorized', 401, special='Authentification required')
@@ -219,11 +155,9 @@ class PlaylistLinks(Resource):
 		pass
 
 class Synchronizer(Resource):
-	@crossdomain(origin='*')
 	def get(self, id):
 		pass
 	
-	@crossdomain(origin='*')
 	def put(self, id):
 		response = request.get_json()
 		if response is None:
@@ -251,7 +185,6 @@ class Synchronizer(Resource):
 		return fk.jsonify({'success': 'Synchronization requested'})
 
 class Links(Resource):
-	@crossdomain(origin='*')
 	def get(self):
 		try:
 			usr = spotify.get('me').json()
